e5d0de1c11a342106d997f1c4052af7b6d64e6f4
auskov-leantech-ai <auskov@leantech.ai>
2021-05-04 23:01:15 +0600

    add socket add started tachometer

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.example.myfirstapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myfirstapp.util.BasicСonstants
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.io.PrintWriter
+import java.net.Socket
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,7 +22,7 @@ class MainActivity : AppCompatActivity() {
 
         val viewPerformance = findViewById<PerformanceView>(R.id.performance)
 
-        viewPerformance.setDataProgressPerformance(92)
+        viewPerformance.setDataProgressPerformance(73)
         viewPerformance.setDataProgressEngLoad(56)
 
         viewDataIntManP.setHeadingData(BasicСonstants.INT_MAN_P)
@@ -36,5 +41,31 @@ class MainActivity : AppCompatActivity() {
         viewDataBarPressure.setCountDoubleData(10.2564343)
         viewDataBarPressure.setTextData(BasicСonstants.BAR)
 
+//        WebSocketHandler().start(this, "ws://192.168.88.213:9081",
+//                onSuccess = { Log.d("WebSocketHandler", it) },
+//                onError = { it.printStackTrace() })
+
+        Thread { wsConnection() }.start()
+    }
+
+    private fun wsConnection() {
+        val hostName = "192.168.88.213"
+        val portNumber = 9081
+
+        Socket(hostName, portNumber).use { kkSocket ->
+            PrintWriter(kkSocket.getOutputStream(), true).use { out ->
+                BufferedReader(
+                        InputStreamReader(kkSocket.getInputStream())).use { `in` ->
+                    out.println("started")
+                    var fromServer: String
+                    while (`in`.readLine().also { fromServer = it } != null) {
+
+                        Log.d("wsConnection", fromServer)
+
+                        out.println("readed");
+                    }
+                }
+            }
+        }
     }
 }
